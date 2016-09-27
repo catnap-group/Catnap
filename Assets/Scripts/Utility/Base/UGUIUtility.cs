01be38e2b6ed0cc9796410e730c996486a4956ff
zouhao <zouhaodata@gmail.com>
2016-09-27 11:27:23 +0800

    【界面】【重新提交商店界面】

---
+++ Assets/Scripts/Utility/Base/UGUIUtility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class UGUIUtility 
+{
+	static public Sprite LoadSprite(string iconPath)
+	{
+		Texture2D texture = Resources.Load<Texture2D> (iconPath);
+		Sprite sprite = Sprite.Create (texture, new Rect (0, 0, texture.width, texture.height), new UnityEngine.Vector2 (0.5f, 0.5f));
+		return sprite;
+	}
+}
