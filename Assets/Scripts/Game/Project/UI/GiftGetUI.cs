9a76bad7a28d70e13f095e42f0c0b17ff073f1d9
maguslin <lchen36@gmail.com>
2016-10-10 18:38:39 +0800

    礼物获得界面

---
+++ Assets/Scripts/Game/Project/UI/GiftGetUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiftGetUI : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	public void PressBack()
+	{
+		Debug.Log ("Press Back");
+		UIManager.Instance.Close (gameObject);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	public void PressHello()
+	{
+		CatSceneManager.Instance.SetNextScene (SceneID.Search);
+	}
+	public void PressCancle()
+	{
+		//CatSceneManager.Instance.SetNextScene (SceneID.Game);
+	}
+}
+
